cb32680026363e4f025917b39f9038fe467e4652
Menightmar <jrma_tyrel@hotmail.com>
2022-07-08 14:53:19 -0500

    Iniciales
    Archivos iniciales.

---
+++ include/Numericos.h
@@ -0,0 +1,42 @@
+
+// Archivo de biblioteca con las clases y metodos para manipulaciones numericas de 2 valores.
+
+// Crearemos la clase principal para los valores numericos.
+
+class Numericos {
+	
+	// Como propiedades privadas tenemos estos valores.
+	
+	private :
+		
+		// Valores de contenido.
+		
+		double Numero_1 , Numero_2;
+	
+	// Ahora los metodos publicos.
+	
+	public :
+		
+		// Constructor por defecto explicito para contruir.
+		
+		explicit Numericos (double Num_1 , double Num_2);
+		
+		// Ahora obtenemos los accesores de datos.
+		
+		// Numero 1 : colocar.
+		
+		void Set_Num_1 (double Num_1);
+		
+		// Numero 1 : Obtener.
+		
+		double Get_Num_1 () const;
+		
+		// Numero 2 : Colocar.
+		
+		void Set_Num_2 (double Num_2);
+		
+		// Numero 1 : Obtener.
+		
+		double Get_Num_2 () const;
+	
+};
---
+++ src/Numericos.cpp
@@ -0,0 +1,61 @@
+
+// Archivo de definición de los númericos.
+
+#include "Numericos.h"
+
+// -----------------------------------------------------------------------------
+
+// Constructor por defecto explicito para contruir.
+
+Numericos::Numericos (double Num_1 , double Num_2) {
+	
+	// Asignamos los valores.
+	
+	Numero_1 = Num_1;
+	Numero_2 = Num_2;
+	
+}
+
+// Ahora definiremos los accesores de datos.
+
+// Numero 1 : colocar.
+
+void Numericos::Set_Num_1 (double Num_1) {
+	
+	// Asignamos el valor.
+	
+	Numero_1 = Num_1;
+	
+}
+
+// Numero 1 : Obtener.
+
+double Numericos::Get_Num_1 () const {
+	
+	// Devolvemos el valor actual.
+	
+	return Numero_1;
+	
+}
+
+// Numero 2 : Colocar.
+
+void Numericos::Set_Num_2 (double Num_2) {
+	
+	// Asignamos el valor.
+	
+	Numero_2 = Num_2;
+	
+}
+
+// Numero 1 : Obtener.
+
+double Numericos::Get_Num_2 () const {
+	
+	// Devolvemos el valor actual.
+	
+	return Numero_2;
+	
+}
+
+// -----------------------------------------------------------------------------
